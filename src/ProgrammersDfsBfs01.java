1f9c8b63f5d7b1db4bc7d270f3e58a3c56aee26c
YSH <sheepsh93@gmail.com>
2021-02-22 16:32:54 +0900

    프로그래머스 Dfs/Bfs(깊이 우선 탐색/너비 우선 탐색) 01번

---
+++ src/ProgrammersDfsBfs01.java
@@ -0,0 +1,42 @@
+class Solution {
+    static int answer = 0;
+    public int solution(int[] numbers, int target) {
+
+
+        dfs(numbers,0,0,target);
+
+        return answer;
+
+    }
+    public static void dfs(int[] numbers,int idx,int sum,int target) {
+
+        if(numbers.length == idx) {
+            if(sum == target) {
+                answer++;
+            }
+        } else {
+            dfs(numbers,idx+1,sum+numbers[idx],target);
+            dfs(numbers,idx+1,sum-numbers[idx],target);
+        }
+    }
+
+}
+
+//다른 사람의 깔끔한 풀이.
+
+//class Solution {
+//    public int solution(int[] numbers, int target) {
+//        int answer = 0;
+//        answer = dfs(numbers, 0, 0, target);
+//        return answer;
+//    }
+//    int dfs(int[] numbers, int n, int sum, int target) {
+//        if(n == numbers.length) {
+//            if(sum == target) {
+//                return 1;
+//            }
+//            return 0;
+//        }
+//        return dfs(numbers, n + 1, sum + numbers[n], target) + dfs(numbers, n + 1, sum - numbers[n], target);
+//    }
+//}
