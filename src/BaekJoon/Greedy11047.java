1fa3c35465ef1c31d385c691c88311302a0f78a4
YSH <sheepsh93@gmail.com>
2021-03-06 14:36:56 +0900

    백준 11047번 동전(Greedy)

---
+++ src/BaekJoon/Greedy11047.java
@@ -0,0 +1,35 @@
+package BaekJoon;
+
+
+import java.util.Scanner;
+
+//백준 11047번 동전
+//https://www.acmicpc.net/problem/11047
+public class Greedy11047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int count = 0;
+        int[] coins = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            coins[i] = sc.nextInt();
+        }
+
+        for (int i = coins.length - 1; i >= 0; i--) {
+
+            if (k == 0) {
+                break;
+            }
+
+            if (k / coins[i] >= 1) {
+                count += k / coins[i];
+                k = k % coins[i];
+            }
+        }
+
+        System.out.println(count);
+    }
+}
