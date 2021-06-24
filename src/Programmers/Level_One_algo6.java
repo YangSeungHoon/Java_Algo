40025103946f3865b85dca6e3c560ce711a48763
YSH <sheepsh93@gmail.com>
2021-06-24 18:01:38 +0900

    프로그래머스 Level 1 -예산

---
+++ src/Programmers/Level_One_algo6.java
@@ -0,0 +1,21 @@
+package Programmers;
+import java.util.Arrays;
+//https://programmers.co.kr/learn/courses/30/lessons/12982
+//예산
+public class Level_One_algo6 {
+
+    class Solution {
+        public int solution(int[] d, int budget) {
+            int answer = 0;
+            Arrays.sort(d);
+            for(int i = 0; i < d.length; i++) {
+                if(budget >= d[i]){
+                    answer++;
+                    budget -= d[i];
+                }
+            }
+            return answer;
+        }
+    }
+
+}
