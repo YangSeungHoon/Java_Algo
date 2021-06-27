01e1591744da146002b095ceef7c0fa57009f8a4
YSH <sheepsh93@gmail.com>
2021-06-27 19:12:19 +0900

    프로그래머스 Level 1 -문자열 내 마음대로 정렬하기

---
+++ src/Programmers/Level_One_algo13.java
@@ -0,0 +1,21 @@
+package Programmers;
+import java.util.*;
+//https://programmers.co.kr/learn/courses/30/lessons/12915
+//문자열 내 마음대로 정렬하기
+public class Level_One_algo13 {
+
+    class Solution {
+        public String[] solution(String[] strings, int n) {
+            ArrayList<String> list = new ArrayList<>();
+            for(int i = 0; i < strings.length; i++) {
+                list.add(strings[i].charAt(n)+strings[i]);
+            }
+            Collections.sort(list);
+            String[] answer = new String[list.size()];
+            for(int i = 0; i < list.size(); i++) {
+                answer[i] = list.get(i).substring(1);
+            }
+            return answer;
+        }
+    }
+}
