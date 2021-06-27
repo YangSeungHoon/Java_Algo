d04e855b300b2660d4590f34bf8c97d3e25ec044
YSH <sheepsh93@gmail.com>
2021-06-27 19:11:19 +0900

    프로그래머스 Level 1 -가운데 글자 가져오기

---
+++ src/Programmers/Level_One_algo9.java
@@ -0,0 +1,19 @@
+package Programmers;
+
+//https://programmers.co.kr/learn/courses/30/lessons/12903
+//가운데 글자 가져오기
+public class Level_One_algo9 {
+    class Solution {
+        public String solution(String s) {
+            String answer = "";
+            if(s.length() % 2 == 1){
+                answer += s.charAt(s.length()/ 2);
+            }else {
+                answer += s.charAt(s.length()/2 - 1);
+                answer += s.charAt(s.length()/2);
+            }
+
+            return answer;
+        }
+    }
+}
