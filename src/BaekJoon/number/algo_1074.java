b9a03647f6514f94cdcd5175d3ab03de27b1ac45
YangSeungHoon <sheepsh93@gmail.com>
2021-07-27 08:51:13 +0900

    백준 1074 - Z

---
+++ src/BaekJoon/number/algo_1074.java
@@ -0,0 +1,47 @@
+package BaekJoon.number;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/1074
+// Z
+public class algo_1074 {
+    static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken()); //행
+        int c = Integer.parseInt(st.nextToken()); //열
+        int size = (int) Math.pow(2, N); //한 변의 사이즈
+
+        find(size, r, c);
+        System.out.println(count);
+    }
+
+    private static void find(int size, int r, int c) {
+        if(size == 1)
+            return;
+
+        if(r < size/2 && c < size/2) { //1사분면
+            find(size/2, r, c);
+        }
+        else if(r < size/2 && c >= size/2) { //2사분면
+            count += size * size / 4;
+            find(size/2, r, c - size/2);
+        }
+        else if(r >= size/2 && c < size/2) { //3사분면
+            count += (size * size / 4) * 2;
+            find(size/2, r - size/2, c);
+        }
+        else {//4사분면
+            count += (size * size / 4) * 3;
+            find(size/2, r - size/2, c - size/2);
+        }
+    }
+}
+
