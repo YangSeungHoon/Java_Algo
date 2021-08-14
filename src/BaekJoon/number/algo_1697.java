d9159fb856bf0b2323dc4b06c01bf6730eb469dd
YangSeungHoon <sheepsh93@gmail.com>
2021-08-14 23:24:59 +0900

    백준 1697 - 숨바꼭질

---
+++ src/BaekJoon/number/algo_1697.java
@@ -0,0 +1,64 @@
+package BaekJoon.number;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+//https://www.acmicpc.net/problem/1697
+//숨바꼭질
+public class algo_1697 {
+
+
+    static int N;
+    static int K;
+    static int second;
+    static Queue<Integer> q;
+    static int[] check = new int[100001];
+
+    public static void input() {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        K = sc.nextInt();
+        second = Integer.MAX_VALUE;
+        q = new LinkedList<>();
+    }
+    public static void main(String[] args) {
+        input();
+
+        bfs(N);
+    }
+
+    public static void bfs(int loc) {
+
+
+        q.add(loc);
+        check[loc] = 1;
+
+        while (!q.isEmpty()) {
+            int temp = q.poll();
+
+            for (int i = 0; i < 3; i++) {
+                int next;
+
+                if (i == 0) {
+                    next = temp + 1;
+                } else if (i == 1) {
+                    next = temp - 1;
+                } else {
+                    next = temp * 2;
+                }
+
+                if (next == K) {
+                    System.out.println(check[temp]);
+                    return;
+                }
+
+                if (next >= 0 && next < check.length && check[next] == 0) {
+                    q.add(next);
+                    check[next] = check[temp] + 1;
+                }
+            }
+        }
+    }
+}
