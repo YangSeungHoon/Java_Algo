7136d580fd8fdc6154779329f3742e783470a6f0
YangSeungHoon <sheepsh93@gmail.com>
2021-08-16 21:16:00 +0900

    백준 1325 - 효율적인 해킹 (메모리 초과 문제 해결)

--- src/BaekJoon/number/algo_1325.java
+++ src/BaekJoon/number/algo_1325.java
@@ -1,37 +1,58 @@
 package BaekJoon.number;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 //https://www.acmicpc.net/problem/1325
 //효율적인 해킹
 public class algo_1325 {
 
+    static class Computer{
+        int idx;
+        ArrayList<Computer> adj;
+
+        public Computer(int idx) {
+            this.idx = idx;
+            this.adj = new ArrayList<>();
+        }
+    }
+
     static int N;
     static int M;
-    static int[][] edge;;
+    static Computer[] comps;
     static int[] counts;
     static int max;
+    static boolean visited[];
     public static void input() {
         Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
         M = sc.nextInt();
         counts = new int[N+1];
-        edge = new int[N+1][N+1];
+        comps = new Computer[N+1];
+        for (int i = 1; i <= N; i++) {
+            comps[i] = new Computer(i);
+        }
+
         for(int i = 0; i < M; i++) {
             int start = sc.nextInt();
             int end = sc.nextInt();
-            edge[start][end] = 1;
+            comps[end].adj.add(comps[start]);
         }
+
         max= Integer.MIN_VALUE;
     }
     public static void main(String[] args) {
         input();
 
+
         for(int i = 1; i <= N; i++) {
-            dfs(i,i);
+            visited = new boolean[N+1];
+            visited[i] = true;
+            dfs(i, i);
             max = Math.max(counts[i],max);
         }
+
         StringBuilder sb = new StringBuilder();
         for(int i= 1; i <= N; i++){
             if(counts[i] == max){
@@ -42,15 +63,15 @@ public class algo_1325 {
 
     }
 
+
     public static void dfs(int cur,int start){
 
-        for(int i = 1; i <= N; i++){
-
-            if(edge[i][start] == 1){
+        for (Computer c : comps[start].adj) {
+            if (!visited[c.idx]) {
+                visited[c.idx] = true;
+                dfs(cur, c.idx);
                 counts[cur]++;
-                dfs(cur,i);
             }
-
         }
     }
 }
