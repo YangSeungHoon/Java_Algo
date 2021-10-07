40a429064cba25948caaccab877cf2b7b8ffbbf2
YangSeungHoon <sheepsh93@gmail.com>
2021-10-07 16:21:01 +0900

    프로그래머스 - 문자열 압축( 코드 재수정)

--- src/Programmers/Level_Two/algo_21.java
+++ src/Programmers/Level_Two/algo_21.java
@@ -43,9 +43,9 @@ public class algo_21 {
             }
 
            enc += prev + last;
-//            if (count != 1) {
-//                enc = count + enc;
-//            }
+            if (count != 1) {
+                enc = count + enc;
+            }
             System.out.println(enc);
 
             answer = Math.min(answer,enc.length());
@@ -56,40 +56,3 @@ public class algo_21 {
 }
 
 
-//
-//    public static int getResult() {
-//
-//        String s = "aabbaccc";
-//        int answer = s.length();
-//
-//        for (int i = 1; i < s.length() / 2 + 1; i++) {
-//            String prev = s.substring(0, i); //0이상 1미만
-//            int count = 1;
-//            String enc = "";
-//            String last = "";
-//            for (int j = i; j < s.length(); j += i) {
-//                if (j + i > s.length()) {
-//                    last = s.substring(j);
-//                    continue;
-//                }
-//                if (prev.equals(s.substring(j, j + i))) {
-//                    count++;
-//                } else {
-//                    enc += prev;
-//                    if (count != 1) {
-//                        enc = count + enc;
-//                    }
-//                    prev = s.substring(j, j + i);
-//                    count = 1;
-//                }
-//            }
-//            enc += prev + last;
-//            if (count != 1) {
-//                enc = count + enc;
-//            }
-//
-//            answer = Math.min(answer, enc.length());
-//        }
-//
-//        return answer;
-//    }
